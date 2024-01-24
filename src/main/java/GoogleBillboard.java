37f5f5f548073b905c9e182cd8a81a20d17f7b4a
willy2day <143228693+willy2day@users.noreply.github.com>
2024-01-23 19:25:05 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -21,11 +21,11 @@ public class GoogleBillboard {
 
 	//Finish this function
 	public boolean isPrime(double d){
-		 if(dNum<2){//Checks the number inside the parameter to show false if the number is less than 2 since it wont be prime if less than 2
+		 if(d<2){//Checks the number inside the parameter to show false if the number is less than 2 since it wont be prime if less than 2
     return false;
   }
-  for(int i = 2; i <= Math.sqrt(dNum); i++){//Starts for loop on 2 because the smallest prime # is 2
-    if(dNum%i == 0){//Checks if the number is even which returns false
+  for(int i = 2; i <= Math.sqrt(d); i++){//Starts for loop on 2 because the smallest prime # is 2
+    if(d%i == 0){//Checks if the number is even which returns false
       return false;
     }
   }
